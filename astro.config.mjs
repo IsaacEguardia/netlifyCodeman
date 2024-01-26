9649d256ce08b141a19239d02701bc48a5040852
IsaacEguardia <isaac.escobar29@gmail.com>
2024-01-26 00:06:19 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,24 @@
-import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify/functions"; // Import the Netlify adapter
 
+// https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
+  site: "https://codemanpty.com", // Update this with your Netlify site domain
+  integrations: [
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: { theme: "github-dark-dimmed" },
+      gfm: true,
+    }),
+    sitemap(),
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  adapter: netlify(), // Use the Netlify adapter
 });
